6a4d37eb29c00a3d86f5889eb1dbad77719fa5e4
Nadim <nadla777@student.liu.se>
2025-02-12 00:00:31 +0100

    teensy project setup with testing code and flashing, makefile

--- project/firmware_singlecore/src/main.rs
+++ project/firmware_singlecore/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use teensy4_bsp as bsp;
+use teensy4_panic as _;
+
+use bsp::board;
+
+#[bsp::rt::entry]
+fn main() -> ! {
+    let board::Resources {
+        pins, mut gpio2, ..
+    } = board::t40(board::instances());
+    let led = board::led(&mut gpio2, pins.p13);
+    loop {
+        led.set();
+    }
 }
