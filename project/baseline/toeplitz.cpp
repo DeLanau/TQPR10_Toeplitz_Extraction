bb8ed5cb77724b27c0e568de80193b2708ce84e9
Love Arreborn <love.arreborn@liu.se>
2025-04-07 11:26:30 +0200

    oops equal sign bad

--- project/baseline/toeplitz.cpp
+++ project/baseline/toeplitz.cpp
@@ -10,18 +10,18 @@ using namespace std;
 vector<bool> toeplitz_extraction(const vector<bool> &raw_bits,
                                  const vector<bool> &seed_bits,
                                  size_t output_len) {
-  size_t n = raw_bits.size();
-  size_t m = output_len;
+  size_t n{raw_bits.size()};
+  size_t m{output_len};
 
   if (seed_bits.size() < m + n - 1) {
-    throw runtime_error("Seed is smaller than m + n - 1");
+    throw runtime_error{"Seed is smaller than m + n - 1"};
   }
 
   vector<bool> output(m, false);
 
-  for (size_t i = 0; i < m; i++) {
+  for (size_t i{0}; i < m; i++) {
     bool sum = false;
-    for (size_t j = 0; j < n; j++) {
+    for (size_t j{0}; j < n; j++) {
       if (raw_bits[j] && seed_bits[i + j]) {
         sum = !sum;
       }
@@ -33,15 +33,15 @@ vector<bool> toeplitz_extraction(const vector<bool> &raw_bits,
 }
 
 int main() {
-  vector<bool> raw_bits = {true, false, true, true, false, true, false, false};
+  vector<bool> raw_bits{true, false, true, true, false, true, false, false};
 
   // Simulating seed bits rather than fetching from real source
   // This should be fetched from sampled data later
   // Constant or variable seed?
-  vector<bool> seed_bits = {true, false, true,  false, true, false,
-                            true, true,  false, true,  false};
+  vector<bool> seed_bits{true, false, true,  false, true, false,
+                         true, true,  false, true,  false};
 
-  vector<bool> extracted_bits = toeplitz_extraction(raw_bits, seed_bits, 4);
+  vector<bool> extracted_bits{toeplitz_extraction(raw_bits, seed_bits, 4)};
 
   cout << "Extracted bits: ";
   for (bool bit : extracted_bits) {
